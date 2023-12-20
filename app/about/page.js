3a366c2392476a569c11adeb96fdc81c9597c4e6
James Serengia <jamesserengia@gmail.com>
2023-12-20 23:29:26 +0300

    Update about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./page.module.scss";
+import Link from "next/link";
 
 export default function page() {
   return (
@@ -12,6 +13,23 @@ export default function page() {
           two numbers. The application stores the data in the cloud using a
           cloud database service.
         </p>
+        <div className={s["links-wrapper"]}>
+          <h3>Source Code:</h3>
+          <div className={s["links"]}>
+            <Link
+              href={"https://github.com/serengia/take-home-backend-20-dec-23"}
+              target="_blank"
+            >
+              Backend API - Node Js
+            </Link>
+            <Link
+              href={"https://github.com/serengia/take-home-frontend-20-dec-23"}
+              target="_blank"
+            >
+              Frontend - React
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
